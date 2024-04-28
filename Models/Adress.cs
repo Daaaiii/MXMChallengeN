aa6bca3b1f6096f3a50e3227ee61490723638c37
Daiane Bolzan <dainha85@gmail.com>
2024-04-27 23:51:36 -0300

    Add project files.

---
+++ Models/Adress.cs
@@ -0,0 +1,17 @@
+﻿namespace MxmChallenge.Models
+{
+    public class Adress
+    {
+        public int Id { get; set; }
+
+        public string Zipcode { get; set; } = null!;
+        public string Street { get; set; } = null!;
+        public string Number { get; set; } = null!;
+        public string? Complement { get; set; }
+        public string Neighborhood { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public Guid UserId { get; set; }
+        public virtual User User { get; set; } = null!;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace MxmChallenge.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Fullname { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+
+        public int DDD { get; set; }
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        public string cpf_cnpj { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public virtual Address Address { get; set; } = new Address();
+
+    }
+}
